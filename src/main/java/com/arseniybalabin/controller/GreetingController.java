ca3fb19676a2af6e3826c2bfecf4d8a3e141f840
Simon <32>
2020-01-16 02:08:26 +0300

    q

--- src/main/java/com/arseniybalabin/controller/GreetingController.java
+++ src/main/java/com/arseniybalabin/controller/GreetingController.java
@@ -8,24 +8,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
 
-
-
 @Controller
 public class GreetingController {
-
-
 
 
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
 
 
-
     public Greeting greeting(HelloMessage message) throws Exception {
 
 
-
-
-        return new Greeting(  "Hello" + HtmlUtils.htmlEscape(message.getName()));
-        }
+        return new Greeting("Hello" + HtmlUtils.htmlEscape(message.getName()));
     }
+}
